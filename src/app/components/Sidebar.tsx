85bc4cc75bf26810a8e1707a69e1e30f961383ef
Luís Mateus <luismateus@Luiss-Air.lan>
2023-05-26 10:08:36 +0100

    First commit

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import IconClose from "./icons/IconClose";
+
+type Props = {
+    children: ReactNode;
+    open: boolean;
+    onClose: () => void;
+  }
+
+export const Sidebar = ({open, onClose, children }: Props) => {
+    return (
+        <section className={`fixed left-0 top-0 bottom-0 text-white 
+        ${open ? 'w-screen bg-gray-600/75' : 'w-0'} md:w-64 md:static`}>
+
+            <div className={`flex h-screen ${open ? 'ml-0' : '-ml-96'}`}>
+
+                <div className="">
+
+                </div>
+
+                <div onClick={onClose} className="flex justify-center items-center w-10 h-10 cursor-pointer md:hidden">
+                    <IconClose width={24} height={24}/>
+
+                </div>
+
+            </div>
+            
+        </section>
+    )
+}
